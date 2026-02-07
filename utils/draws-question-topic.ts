3e5879d97baebf284f522e60c0f0ce402454e32a
Davi Dias <davifernandosousadias18@gmail.com>
2026-02-07 16:27:37 -0300

    fix: change to the business rule to switch questions based on the number of errors.

--- utils/draws-question-topic.ts
+++ utils/draws-question-topic.ts
@@ -1,12 +1,12 @@
-export function switchTopicAfterTwoConsecutiveErrors(currentTopic: Topic) {
-  const topics = [
-    'fundamental_cloud_concepts',
-    'security_compliance',
-    'cloud_technology',
-    'billing_pricing_support',
-  ];
+export const ALL_TOPICS: Topic[] = [
+  'fundamental_cloud_concepts',
+  'security_compliance',
+  'cloud_technology',
+  'billing_pricing_support',
+];
 
-  const availableTopics = topics.filter((topic) => topic !== currentTopic);
+export function switchTopicAfterTwoConsecutiveErrors(currentTopic: Topic) {
+  const availableTopics = ALL_TOPICS.filter((topic) => topic !== currentTopic);
 
   const randomIndex = Math.floor(Math.random() * availableTopics.length);
   return availableTopics[randomIndex];
